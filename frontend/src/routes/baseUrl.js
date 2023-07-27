a885c50a040abb4fbfd5bd81e40c2f62fb3812e9
Shaikhaltamash861 <altamash2c2@gmail.com>
2023-07-27 13:23:55 +0530

    ready

--- frontend/src/routes/baseUrl.js
+++ frontend/src/routes/baseUrl.js
@@ -1,4 +1,5 @@
-const url='https://server-insta-ymwc.onrender.com';
+// const url='https://server-insta-ymwc.onrender.com';
+const url='https://inst-frontend-su4x.vercel.app'
 // const url='http://localhost:8000'
 
 export default url
